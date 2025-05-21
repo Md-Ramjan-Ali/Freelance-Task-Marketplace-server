f2dfc8eba1f35996fe4bce3bb1f91089d9446045
ramjan88 <gmramjanali8888@gmail.com>
2025-05-22 01:51:11 +0600

    added update db

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app=express()
-const port=process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_FREELANCER_USER}:${process.env.DB_FREELANCER_PASSWORD}@brainbazzdatabase.ihuzu7m.mongodb.net/?retryWrites=true&w=majority&appName=BrainBazzDatabase`;
 
@@ -41,12 +33,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/tasks/:id',async(req,res)=>{
-      const id =req.params.id
-      const query={_id: new ObjectId(id)}
-      const result=await taskCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
 
     //post the task data
     app.post("/tasks", async (req, res) => {
@@ -55,13 +47,28 @@ async function run() {
       res.send(result);
     });
 
+    //update db
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateTask = req.body;
+
+      const updateDoc = {
+        $set: updateTask,
+      };
+
+      const result = await taskCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
     //delete the on db
-    app.delete('/tasks/:id',async(req,res)=>{
-      const id =req.params.id
-      const query={_id: new ObjectId(id)}
-      const result= await taskCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -75,20 +82,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Freelancer server in running");
+});
 
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res)=>{
-  res.send('Freelancer server in running')
-})
-
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Freelancer server port ${port}`);
-})
+});
