35a9037a77f5aa4575fb955cadb2c8686fc4ecdb
ramjan88 <gmramjanali8888@gmail.com>
2025-05-21 23:50:59 +0600

    added delete methood and findone methood

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app=express()
 const port=process.env.PORT || 5000
 
@@ -32,41 +32,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const taskCollection = client.db("jobPondDB").collection('tasks');
+    const taskCollection = client.db("jobPondDB").collection("tasks");
 
     //get data on db
-    app.get('/tasks',async(req,res)=>{
-      const cursor=taskCollection.find()
-      const result=await cursor.toArray()
+    app.get("/tasks", async (req, res) => {
+      const cursor = taskCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/tasks/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id: new ObjectId(id)}
+      const result=await taskCollection.findOne(query)
       res.send(result)
     })
 
     //post the task data
-    app.post('/tasks',async(req,res)=>{
-      const newTask=req.body
-      const result=await taskCollection.insertOne(newTask)
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
+    });
+
+    //delete the on db
+    app.delete('/tasks/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id: new ObjectId(id)}
+      const result= await taskCollection.deleteOne(query)
       res.send(result)
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
